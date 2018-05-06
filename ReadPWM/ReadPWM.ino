e8ae764e929adc7778704b61e5908748646aace1
arianekrumel <arianekrumel@gmail.com>
2018-05-06 16:23:55 -0700

    Create script to handle Arduino receiving data from transmitter

---
+++ ReadPWM/ReadPWM.ino
@@ -0,0 +1,28 @@
+/*
+ * ReadPWM
+ * 
+ * This is an Arduino application that connects with
+ * controller through receiver system
+ * 
+ * The circuit:
+ * * Arduino Mega2560
+ * * Fly Sky FS-T6 Controller
+ * * Fly Sky FS-R6B
+ * 
+ * 5/6/18
+ * Ariane Krumel
+ * 
+ * 
+ * Based on https://www.youtube.com/watch?v=v_N5HmXmDyk
+ */
+
+void setup() {
+  pinMode(3, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int v = pulseIn(3, HIGH, 25000);
+  Serial.println(v);
+  delay(500);
+}
