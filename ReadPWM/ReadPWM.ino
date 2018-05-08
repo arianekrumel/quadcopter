7134cd3973e3d83760da2f47c5a963536bf8ab9a
arianekrumel <arianekrumel@gmail.com>
2018-05-07 21:04:27 -0700

    Create flight controller code for thrust, pitch, roll and yaw

--- ReadPWM/ReadPWM.ino
+++ ReadPWM/ReadPWM.ino
@@ -1,20 +1,21 @@
 /*
- * ReadPWM
- * 
- * This is an Arduino application that connects with
- * controller through receiver system
- * 
- * The circuit:
+   ReadPWM
+
+   This is an Arduino application that connects with
+   controller through receiver system
+
+   The circuit:
  * * Arduino Mega2560
  * * Fly Sky FS-T6 Controller
  * * Fly Sky FS-R6B
- * 
- * 5/6/18
- * Ariane Krumel
- * 
- * 
- * Based on https://www.youtube.com/watch?v=v_N5HmXmDyk
- */
+
+   5/6/18
+   Ariane Krumel
+
+
+   Based on https://www.youtube.com/watch?v=v_N5HmXmDyk
+*/
+
 
 void setup() {
   pinMode(3, INPUT);
@@ -25,4 +26,7 @@ void loop() {
   int v = pulseIn(3, HIGH, 25000);
   Serial.println(v);
   delay(500);
+  //Serial.println("Test");
 }
+
+
